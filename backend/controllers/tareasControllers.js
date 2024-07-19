84aaa012499e820d65b987c27dc4a52a1365d343
Erick Jimenez <erick-rubi@hotmail.com>
2024-07-18 20:33:14 -0600

    Fin día 1

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -2,6 +2,21 @@ const getTareas = (req, res) => {
     res.status(200).json({ message: 'getTareas' })
 }
 
+const crearTareas = (req, res) => {
+    res.status(201).json({ message: 'crearTareas' })
+}
+
+const updateTareas = (req, res) => {
+    res.status(201).json({ message: `Modificar la tarea ${req.params.id}` })
+}
+
+const deleteTareas = (req, res) => {
+    res.status(201).json({ message: `Eliminar la tarea ${req.params.id}` })
+}
+
 module.exports = {
-    getTareas
+    getTareas,
+    crearTareas,
+    updateTareas,
+    deleteTareas
 }
